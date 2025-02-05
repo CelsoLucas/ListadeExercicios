f809280b960fcdb0d518392eb09c68f3b51df641
CelsoLucas <lucascelso23@gmail.com>
2025-02-05 17:50:53 -0300

    commit
    commit

--- ex_pyside/ex1.py
+++ ex_pyside/ex1.py
@@ -5,12 +5,13 @@ import sys
 class Celso(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My App")
-        label = QLabel("Celso")
+        self.setWindowTitle("Tela com nome centralizado")
+        self.setGeometry(100, 100, 400, 200)
+
+        label = QLabel("Celso", self)
+        label.setStyleSheet("font-size: 18px; font-weight: bold; text-align: center")
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setCentralWidget(label)
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- ex_pyside/ex2.py
+++ ex_pyside/ex2.py
@@ -1,29 +1,31 @@
-import sys
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
+import sys
 
-class MainWindow(QWidget):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Janela com Nome centralizado e Imagem")
+        self.setGeometry(100, 100, 400, 400)
+        
+        central_widget = QWidget(self)
+        layout = QVBoxLayout(central_widget)
 
-        self.setWindowTitle("Janela com Nome e Imagem")
-        self.setGeometry(100, 100, 400, 300)
+        label = QLabel("Celso", self)
+        label.setStyleSheet("font-size: 24px; font-weight: bold; text-align: center;")
+        label.setAlignment(Qt.AlignCenter)
 
-        layout = QVBoxLayout()
+        img_label = QLabel(self)
+        pixmap = QPixmap(r"ex_pyside\audirs6.jpg")
+        pixmap = pixmap.scaled(400,400)
+        img_label.setPixmap(pixmap)
+        img_label.setAlignment(Qt.AlignCenter)
 
-        nome_label = QLabel("Meu Nome")
-        nome_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        imagem_label = QLabel()
-        pixmap = QPixmap("ex_pyside/audirs6.jpg")  
-        imagem_label.setPixmap(pixmap)
-        imagem_label.setAlignment(Qt.AlignCenter)
-
-        layout.addWidget(nome_label)
-        layout.addWidget(imagem_label)
-
-        self.setLayout(layout)
+        layout.addWidget(label)
+        layout.addWidget(img_label)
+        self.setCentralWidget(central_widget)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
