a45ae56d6855624948afd5750571c79ddafae6b8
CelsoLucas <lucascelso23@gmail.com>
2025-02-07 15:43:40 -0300

    commit
    commit

--- ex_pyside/ex4.py
+++ ex_pyside/ex4.py
@@ -17,21 +17,21 @@ class AnimalWindow(QMainWindow):
         self.dog_button.clicked.connect(self.show_dog)
         layout.addWidget(self.dog_button)
 
-        self.cat_button = QPushButton("Gato")
-        self.cat_button.clicked.connect(self.show_cat)
-        layout.addWidget(self.cat_button)
+        self.botao_gato = QPushButton("Gato")
+        self.botao_gato.clicked.connect(self.show_cat)
+        layout.addWidget(self.botao_gato)
 
-        self.image_label = QLabel()
-        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(self.image_label)
+        self.imagem_label = QLabel()
+        self.imagem_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.imagem_label)
 
     def show_dog(self):
         pixmap = QPixmap("ex_pyside\cachorro.jpg") 
-        self.image_label.setPixmap(pixmap.scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio))
+        self.imagem_label.setPixmap(pixmap.scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio))
 
     def show_cat(self):
         pixmap = QPixmap("ex_pyside\gato.webp")
-        self.image_label.setPixmap(pixmap.scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio))
+        self.imagem_label.setPixmap(pixmap.scaled(300, 300, Qt.AspectRatioMode.KeepAspectRatio))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
